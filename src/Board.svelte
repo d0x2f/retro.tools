1586a019bc4d803ba78aa09586fae5df33247105
Dylan McGannon <peltium@gmail.com>
2020-01-30 21:56:28 +1100

    check when the page is hidden

--- src/Board.svelte
+++ src/Board.svelte
@@ -45,13 +45,20 @@
     }
   })();
 
+  let hidden = false;
+
   async function update() {
-    const [b, c] = await Promise.all([
-      getBoard($board.id),
-      getCards($board.id),
-    ]);
-    board.set(b);
-    cards.set(c);
+    if (!hidden) {
+      document.title = 'not hidden';
+      const [b, c] = await Promise.all([
+        getBoard($board.id),
+        getCards($board.id),
+      ]);
+      board.set(b);
+      cards.set(c);
+    } else {
+      document.title = 'hidden';
+    }
   }
 
   onMount(async () => {
@@ -64,6 +71,10 @@
         previousBoard = { ...b };
       });
     setInterval(async () => await update(), 10000);
+    document.addEventListener('visibilitychange', () => {
+      hidden = document['hidden'];
+      update();
+    });
   });
   onDestroy(() => unsubscribe && unsubscribe());
 
