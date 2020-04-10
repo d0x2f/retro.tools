cfc7113acb0d5b6c5074cdb2d06abfe5cbbaf8a2
chrisleekr <chrisleekr@users.noreply.github.com>
2020-04-10 14:55:51 +1000

    Added Korean locale (#62)
    * Added .vscode
    
    * Added Korean locale
    
    Co-authored-by: Chris Lee <chris@chrislee.kr>

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,11 @@
 import { addMessages, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
 import moment from 'moment';
 import 'moment/locale/es';
+import 'moment/locale/ko';
 
 import en from './lang/en.json';
 import es from './lang/es.json';
+import ko from './lang/ko.json';
 
 locale.subscribe(l => {
   if (l) moment.locale(l);
@@ -11,6 +13,7 @@ locale.subscribe(l => {
 
 addMessages('en', en);
 addMessages('es', es);
+addMessages('ko', ko);
 
 init({
   fallbackLocale: 'en',
