92e5b768a2eb28a6c1a457d457f786a463e5413d
Dylan McGannon <d0x2f@users.noreply.github.com>
2020-07-06 22:26:45 +1000

    cachebust and minify (#72)
    * cachebust and minify
    
    * public -> build dir

--- rollup.config.js
+++ rollup.config.js
@@ -1,55 +1,45 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
+import livereload from "rollup-plugin-livereload";
+import serve from "rollup-plugin-serve";
+import copy from "rollup-plugin-copy";
 import json from '@rollup/plugin-json';
 
-const production = !process.env.ROLLUP_WATCH;
+const IS_PROD = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/build/bundle.js'
+    sourcemap: !IS_PROD,
+    format: "iife",
+    name: "app",
+    file: "build/bundle.[hash].js",
   },
   plugins: [
+    copy({ targets: [{ src: "public/*", dest: "build" }] }),
+
     svelte({
-      dev: !production,
-      css: css => {
-        css.write('public/build/bundle.css');
-      }
+      dev: !IS_PROD,
+      css: (css) => {
+        css.write("build/bundle.[hash].css", !IS_PROD);
+      },
     }),
-    resolve({
-      browser: true,
-      dedupe: ['svelte']
-    }),
+
+    resolve(),
+
     commonjs(),
     json(),
-    !production && serve(),
-    !production && livereload('public'),
-    production && terser()
+
+    !IS_PROD &&
+      serve({
+        contentBase: ["build"],
+        port: 3000,
+      }),
+
+    !IS_PROD && livereload({ watch: "build" }),
+
+    IS_PROD && terser(),
   ],
-  watch: {
-    clearScreen: false
-  }
 };
-
-function serve() {
-  let started = false;
-
-  return {
-    writeBundle() {
-      if (!started) {
-        started = true;
-
-        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-          stdio: ['ignore', 'inherit', 'inherit'],
-          shell: true
-        });
-      }
-    }
-  };
-}
