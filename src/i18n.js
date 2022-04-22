52548495c7e405b16d010dc10c4d2d84ea492579
Ricardo Campos <ricardo@ricardocampos.blog>
2022-04-22 11:56:07 -0300

    feat: adding brazilian portuguese translation

--- src/i18n.js
+++ src/i18n.js
@@ -10,6 +10,7 @@ import es from './lang/es.json';
 import ko from './lang/ko.json';
 import de from './lang/de.json';
 import ru from './lang/ru.json';
+import pt_BR from './lang/pt_BR.json';
 
 locale.subscribe((l) => {
   if (l) moment.locale(l);
@@ -20,6 +21,7 @@ addMessages('es', es);
 addMessages('ko', ko);
 addMessages('de', de);
 addMessages('ru', ru);
+addMessages('pt_BR', pt_BR);
 
 init({
   fallbackLocale: 'en',
