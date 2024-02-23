a426bee0fe2cd10dc034d27ebae25336af9b7f1d
Ahmet Keskin <a.keskin@yaani.com>
2024-02-23 23:59:05 +0300

    The Turkish language file has been added

---
+++ i18n.js
@@ -0,0 +1,32 @@
+import { addMessages, init, getLocaleFromNavigator, locale } from "svelte-i18n";
+import moment from "moment";
+import "moment/locale/es";
+import "moment/locale/ko";
+import "moment/locale/de";
+import "moment/locale/ru";
+
+import en from "./lang/en.json";
+import es from "./lang/es.json";
+import ko from "./lang/ko.json";
+import de from "./lang/de.json";
+import ru from "./lang/ru.json";
+import pt_BR from "./lang/pt_BR.json";
+import pt_BR from "./lang/tr.json";
+
+locale.subscribe((l) => {
+  if (l) moment.locale(l);
+});
+
+addMessages("en", en);
+addMessages("es", es);
+addMessages("ko", ko);
+addMessages("de", de);
+addMessages("ru", ru);
+addMessages("pt_BR", pt_BR);
+addMessages("tr", tr);
+
+init({
+  fallbackLocale: "en",
+  initialLocale:
+    window.localStorage.getItem("locale") || getLocaleFromNavigator(),
+});
