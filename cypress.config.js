5407bb6781c10a1902f9f3ca58eab8c1cc813718
Dylan <dylan@encapto.com>
2026-04-11 21:06:17 +0100

    update cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   defaultCommandTimeout: 15000,
+  allowCypressEnv: false,
   e2e: {
     baseUrl: "http://localhost:5173",
   },
